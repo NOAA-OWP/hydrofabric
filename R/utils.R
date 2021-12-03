d0c85dff4ff09612396042a2f43a3c2e78e11105
mikejohnson51 <jmj00@ucsb.edu>
2021-12-02 20:30:56 -0700

    meta package start

---
+++ R/utils.R
@@ -0,0 +1,59 @@
+msg <- function(..., startup = FALSE) {
+  if (startup) {
+    if (!isTRUE(getOption("hydrofabric.quiet"))) {
+      packageStartupMessage(text_col(...))
+    }
+  } else {
+    message(text_col(...))
+  }
+}
+
+text_col <- function(x) {
+  # If RStudio not available, messages already printed in black
+  if (!rstudioapi::isAvailable()) {
+    return(x)
+  }
+  
+  if (!rstudioapi::hasFun("getThemeInfo")) {
+    return(x)
+  }
+  
+  theme <- rstudioapi::getThemeInfo()
+  
+  if (isTRUE(theme$dark)) crayon::white(x) else crayon::black(x)
+  
+}
+
+#' List all packages in the hydrofabric
+#'
+#' @param include_self Include hydrofabric in the list?
+#' @export
+#' @examples
+#' hydrofabric_packages()
+
+hydrofabric_packages <- function(include_self = TRUE) {
+  raw <- utils::packageDescription("hydrofabric")$Imports
+  imports <- strsplit(raw, ",")[[1]]
+  parsed <- gsub("^\\s+|\\s+$", "", imports)
+  names <- vapply(strsplit(parsed, "\\s+"), "[[", 1, FUN.VALUE = character(1))
+  
+  if (include_self) {
+    names <- c(names, "hydrofabric")
+  }
+  
+  names
+}
+
+invert <- function(x) {
+  if (length(x) == 0) return()
+  stacked <- utils::stack(x)
+  tapply(as.character(stacked$ind), stacked$values, list)
+}
+
+
+style_grey <- function(level, ...) {
+  crayon::style(
+    paste0(...),
+    crayon::make_style(grDevices::grey(level), grey = TRUE)
+  )
+}
