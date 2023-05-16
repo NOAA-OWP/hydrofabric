06c6371a68984abf5afa4957d3ba369fdb8cdd1c
mikejohnson51 <jmj00@ucsb.edu>
2023-05-16 11:57:08 -0600

    dev workshop pkgdown

--- R/subset_network.R
+++ R/subset_network.R
@@ -10,6 +10,66 @@
 #' @return file path or list 
 #' @export
 
+subset_reference = function(nldi_feature,
+                            gpkg = NULL,
+                            pattern = '/Volumes/Transcend/ngen/CONUS-hydrofabric/01_reference/reference_{vpu}.gpkg',
+                            export_gpkg = NULL) {
+  
+  feat =  get_nldi_feature(nldi_feature)
+  vpu =  st_join(feat, st_transform(vpu_boundaries, st_crs(feat))) %>% 
+    pull(VPUID)
+  
+  if(is.null(gpkg)){
+    gpkg = glue(pattern, vpu = vpu)
+  }
+  
+
+  flowpaths = grep("flowpath|flowline", st_layers(gpkg)$name, value = TRUE)
+  flowpaths = flowpaths[!grepl("attributes|edge_list", flowpaths)]
+  
+  if (length(flowpaths) > 1) {
+    stop("Multiple flowpath names found.")
+  }
+  
+  catchments = grep("divide|catchment", st_layers(gpkg)$name, value = TRUE)
+  catchments = catchments[!grepl("network", catchments)]
+  if (length(catchments) > 1) {
+    stop("Multiple catchment names found.")
+  }
+  
+  UT_COMIDs <-  navigate_nldi(nldi_feature = nldi_feature, mode = "UT", distance_km = 1000)$UT$nhdplus_comid
+  
+  id_list      <- paste0("'", UT_COMIDs, "'", collapse = ",")
+  
+  query_suffix <- paste(glue("COMID IN ({id_list})"), collapse = " OR ")
+  query        <- glue("SELECT * FROM {flowpaths} WHERE {query_suffix}")
+  flowlines            <- st_read(gpkg, query = query, quiet = TRUE)
+  
+  query_suffix <- paste(glue("FEATUREID IN ({id_list})"), collapse = " OR ")
+  query        <- glue("SELECT * FROM {catchments} WHERE {query_suffix}")
+  catchments           <- st_read(gpkg, query = query, quiet = TRUE)
+  
+  
+  if(!is.null(export_gpkg)){
+    write_hydrofabric(list(flowlines = flowlines, catchments = catchments), export_gpkg, enforce_dm = FALSE)
+  } else {
+    return(list(flowpaths = flowlines, catchments = catchments))
+  }
+}
+
+
+#' Subset Hydrofabric Network
+#' @param id hydrofabric id
+#' @param comid NHDPlusV2 COMID
+#' @param hl_id Hydrolocation URI
+#' @param network Network Parquet file
+#' @param pattern Pattern for distributed VPU based GPKGS
+#' @param lyrs layers to extract
+#' @param export_gpkg file path to write to
+#'
+#' @return file path or list 
+#' @export
+
 subset_network = function(id = NULL, 
                           comid = NULL, 
                           hl_id = NULL,
@@ -27,7 +87,8 @@ subset_network = function(id = NULL,
   
   if(!is.null(comid)){
     origin = filter(net, hf_id == comid) %>% 
-      slice_min(hydroseq) %>% 
+      slice_max(hydroseq) %>% 
+      # We want everthing that flows to this ID
       pull(id) %>% 
       unique()
   }
@@ -35,23 +96,29 @@ subset_network = function(id = NULL,
   if(!is.null(hl_id)){
     origin = filter(net, hl_uri == !!hl_id) %>% 
       slice_max(hydroseq) %>% 
-      pull(id) %>% 
+      # We want everything that flows to this HL, HL are nexus.
+      pull(toid) %>% 
       unique()
   }
   
   if(!is.null(id)){ origin = id }
   
   message("Starting from: `",  origin, "`")
-  
-  #toid = unique(pull(filter(net, id == !!origin), toid))
-  
+
   vpuid = unique(pull(filter(net, id == origin | toid == origin), vpu))
   
   sub_net = select(filter(net, vpu == vpuid), -vpu)
 
-  ids = unique(c(unlist(get_sorted(
-    distinct(select(sub_net, id, toid)), outlets = origin
-  ))))
+  tmap = get_sorted(
+    distinct(select(sub_net, id, toid)), 
+    outlets = origin
+  )
+  
+  if(!grepl("nex-", tmap$toid[nrow(tmap)])){
+    tmap = tmap[-nrow(tmap),]
+  }
+  
+  ids = unique(c(unlist(tmap)))
   
   gpkg = glue(pattern, vpu = vpuid)
   
@@ -66,7 +133,7 @@ subset_network = function(id = NULL,
     crs = st_layers(gpkg)$crs
     
     t = tbl(db, lyrs[j]) %>%
-      filter(if_any(contains('id'), ~ . %in% ids)) %>%
+      filter(if_any(any_of(c('divide_id', 'id', 'ds_id')), ~ . %in% ids)) %>%
       collect()
     
     if(all(!any(is.na(as.character(crs[[j]]))), nrow(t) > 0 )){
--- data/data-raw/net_parquets.R
+++ data/data-raw/net_parquets.R
@@ -1,6 +1,11 @@
 
 # Build National Topology -------------------------------------------------
 library(sf); library(arrow)
-"/Volumes/Transcend/ngen/CONUS-hydrofabric/05_nextgen/conus.gpkg" %>% 
+vaa = get_vaa("hydroseq") %>% 
+  rename(hf_id = comid, hf_hydroseq = hydroseq)
+
+xx = "/Volumes/Transcend/ngen/CONUS-hydrofabric/05_nextgen/conus.gpkg" %>% 
   read_sf('network') %>% 
-  write_parquet("data/conus_net.parquet")
+  left_join(vaa, by = "hf_id")
+  
+write_parquet(xx, "data/conus_net.parquet")
