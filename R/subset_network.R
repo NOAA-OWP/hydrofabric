571716b0dbc08ab5068d47c052cfd2037d29c1dd
mikejohnson51 <jmj00@ucsb.edu>
2023-10-17 12:55:31 -0600

    move append_styles

--- R/subset_network.R
+++ R/subset_network.R
@@ -1,3 +1,62 @@
+read_qml = function (qml_file)  {
+  paste(readLines(qml_file), collapse = "\n")
+}
+
+create_style_row = function (gpkg_path,
+                             layer_name,
+                             style_name,
+                             style_qml) {
+  geom_col <-
+    sf::st_read(
+      gpkg_path,
+      query = paste0(
+        "SELECT column_name from gpkg_geometry_columns ",
+        "WHERE table_name = '",
+        layer_name,
+        "'"
+      ),
+      quiet = TRUE
+    )[1,
+      1]
+  data.frame(
+    f_table_catalog = "",
+    f_table_schema = "",
+    f_table_name = layer_name,
+    f_geometry_column = geom_col,
+    styleName = style_name,
+    styleQML = style_qml,
+    styleSLD = "",
+    useAsDefault = TRUE,
+    description = "Generated for hydrofabric",
+    owner = "",
+    ui = NA,
+    update_time = Sys.time()
+  )
+}
+
+append_style = function (gpkg_path, qml_dir = system.file("qml", package = "hydrofabric"), layer_names) {
+  
+    f = list.files(qml_dir, full.names = TRUE)
+    
+    good_layers = gsub(".qml", "", basename(f))
+
+    layer_names = layer_names[layer_names %in% good_layers]
+    
+    files = grep(paste(layer_names, collapse = "|"), f, value = TRUE)
+    
+    styles <- sapply(files, read_qml)
+    style_names <- sapply(layer_names, paste0, "__hydrofabric_style")
+    style_rows <- do.call(rbind, mapply(create_style_row, layer_names, 
+                                        style_names, styles, MoreArgs = list(gpkg_path = gpkg_path), 
+                                        SIMPLIFY = FALSE))
+    if ("layer_styles" %in% sf::st_layers(gpkg_path)$name) {
+      try(st_delete(gpkg_path, "layer_styles"), silent = TRUE)
+    }
+    st_write(style_rows, gpkg_path, layer = "layer_styles", append = FALSE, 
+             quiet = TRUE)
+    return(gpkg_path)
+}
+
 #' Access Hydrofabric Network
 #' @param VPU Vector Processing Unit
 #' @param base_s3 the base hydrofabric directory to access in Lynker's s3
