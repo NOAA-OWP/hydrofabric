9c7386dae339823ffcfb1ef85bce5d112c3c7da3
mikejohnson51 <jmj00@ucsb.edu>
2024-05-28 00:00:35 -0600

    devcon2924: example script

--- runners/devcon2024-example.R
+++ runners/devcon2024-example.R
@@ -4,6 +4,7 @@
 library(hydrofabric)
 library(powerjoin)
 
+# Helper Function ---------------------------------------------------------
 
 make_map = function(file, pois) {
   hf = read_hydrofabric(file)
@@ -12,6 +13,8 @@ make_map = function(file, pois) {
 
 ### ---- Sample out files and source for today ---- ###
 fs::dir_create("tutorial")
+
+# Setup Needed File Structure ---------------------------------------------
 
 source    <- '/Users/mjohnson/hydrofabric/'
 
@@ -22,6 +25,8 @@ aggregated_file <- "tutorial/aggregated.gpkg"
 nextgen_file       <- "tutorial/poudre_ng.gpkg"
 model_atts_file    <- "tutorial/poudre_ng_attributes.parquet"
 model_weights_file <- "tutorial/poudre_ng_weights.parquet"
+
+# Extract Subset of Reference Fabric --------------------------------------
 
 get_subset(
   hl_uri = "Gages-06752260",
@@ -35,6 +40,8 @@ get_subset(
 
 hf = read_hydrofabric(reference_file)
 
+# Pull out community POIs for Subset --------------------------------------
+
 pois = open_dataset(glue("{source}/v2.2/conus_hl")) %>%
   filter(hl_source == 'GFv20', 
          vpuid %in% unique(hf$flowpaths$vpuid),
@@ -43,6 +50,9 @@ pois = open_dataset(glue("{source}/v2.2/conus_hl")) %>%
   st_as_sf(coords = c("X", "Y"), crs = 5070)
 
 make_map(reference_file, pois)
+
+
+# Build Refactored Fabric -------------------------------------------------
 
 refactored = refactor(
   reference_file,
@@ -57,10 +67,16 @@ refactored = refactor(
 
 make_map(refactored_file, pois)
 
+
+# Remap Hydrolocations ----------------------------------------------------
+
 hydrolocations = read_sf(refactored_file, 'lookup_table') %>%
   inner_join(pois, by = c("NHDPlusV2_COMID" = "hf_id")) %>%
   select(poi_id, NHDPlusV2_COMID, id = reconciled_ID) %>%
   distinct()
+
+
+# Aggregate Network -------------------------------------------------------
 
 aggregate_to_distribution(
   gpkg = refactored_file,
@@ -73,45 +89,59 @@ aggregate_to_distribution(
 
 make_map(aggregated_file, pois)
 
+
+# Apply NextGen Topology and Structure ------------------------------------
+
 unlink(nextgen_file)
 apply_nexus_topology(aggregated_file, export_gpkg = nextgen_file)
 
-hf = read_hydrofabric(nextgen_file)
-                      
 make_map(nextgen_file, read_sf(nextgen_file, "nexus"))
-
-
-And there you have it! This is the minimal set of information needed in a NextGen hydrofabric!
 
 # Enriching the Network
 
+# Enrich your new network --------------------------------------------------
 
+# LS Hooks for gridded data
 vsi <- "/vsis3/lynker-spatial/gridded-resources"
+
+# Divides for NextGen Fabric
 div <- read_sf(nextgen_file, "divides")
 
+# NOAH OWP Variables (Grid -> Polygon)
 nom_vars <- c("bexp", "dksat", "psisat", "smcmax", "smcwlt")
 
-r = rast(glue("{vsi}/nwm/conus/{nom_vars}.tif"), lyrs = seq(1,length(nom_vars)*4, by = 4))
+# Read the first layer of each from the remote COG Store
+# No need oto crop here as only the connection is being established
+r = rast(glue("{vsi}/nwm/conus/{nom_vars}.tif"), 
+         lyrs = seq(1,length(nom_vars)*4, by = 4))
 
+# Get Mode Beta parameter
 modes = execute_zonal(r[[1]], 
                     fun = mode,
                     div, ID = "divide_id", 
                     join = FALSE)  %>% 
     setNames(gsub("fun.", "", names(.)))
 
+# Get Geometric Mean of Saturated soil hydraulic conductivity, 
+# and matric potential
 gm = execute_zonal(r[[2:3]], 
                     fun = geometric_mean,
                     div, ID = "divide_id", 
                     join = FALSE)  %>% 
     setNames(gsub("fun.", "", names(.)))
 
+# Get Mean Saturated value of soil moisture and Wilting point soil moisture
 m = execute_zonal(r[[4:5]], 
                     fun = "mean",
                     div, ID = "divide_id", 
                     join = FALSE)  %>% 
     setNames(gsub("mean.", "", names(.)))
 
+
+# Merge all tables into one
 d1 <- power_full_join(list(modes, gm, m), by = "divide_id")
+
+# GWBUCKET Rescale Data  (Polygon --> Polygon) --------------------------------
 
 crosswalk <- as_sqlite(nextgen_file, "network") |>
     select(hf_id, divide_id) |>
@@ -123,11 +153,18 @@ d2 <- open_dataset(glue("{source}/v2.2/reference/conus_routelink")) |>
     group_by(divide_id) |>
     collect() |>
     summarize(
-      gw_Coeff = round(weighted.mean(gw_Coeff, w = gw_Area_sqkm, na.rm = TRUE), 9),
-      gw_Zmax_mm  = round(weighted.mean(gw_Zmax_mm,  w = gw_Area_sqkm, na.rm = TRUE), 9),
+      gw_Coeff = round(weighted.mean(gw_Coeff, 
+                                     w = gw_Area_sqkm, 
+                                     na.rm = TRUE), 9),
+      gw_Zmax_mm  = round(weighted.mean(gw_Zmax_mm,  
+                                        w = gw_Area_sqkm, 
+                                        na.rm = TRUE), 9),
       gw_Expon = mode(floor(gw_Expon))
     )
 
+# Downscaleing Variables
+# 
+# 
 d3 <- st_centroid(div) |>
   st_transform(4326) |>
   st_coordinates() |>
@@ -148,18 +185,26 @@ d5 <- execute_zonal(r[[3]],
                      join = FALSE) |>
     setNames(c("divide_id", "aspect_c_mean"))
 
+
 model_attributes <- power_full_join(list(d1, d2, d3, d4, d5), by = "divide_id")
   
-write_parquet(model_attributes, model_atts_file)
+# Build a Forcing Weights File --------------------------------------------
 
 type = "medium_range.forcing"
 
 w = weight_grid(rast(glue('{vsi}/{type}.tif')), div, ID = "divide_id") |> 
   mutate(grid_id = type)
 
-head(w)
-
+# Write it all out --------------------------------------------------------
 write_parquet(w, model_weights_file)
+write_parquet(model_attributes, model_atts_file)
+
+
+
+
+
+# Enhance the flow network ------------------------------------------------
+
 
 crosswalk <- as_sqlite(nextgen_file, "network") |>
     select(hf_id, id, divide_id, hydroseq, poi_id) |>
@@ -169,7 +214,6 @@ crosswalk <- as_sqlite(nextgen_file, "network") |>
 
 open_dataset(glue("{source}/v2.2/reference/conus_routelink/")) |>
     select(hf_id, starts_with("ml_")) 
-
 
 (cs <- open_dataset(glue("{source}/v2.2/reference/conus_routelink/")) |>
     select(hf_id, ml_y_bf_m, ml_tw_bf_m, ml_r) %>% 
@@ -217,6 +261,12 @@ add_flowpath_attributes(nextgen_file, source = source)
 as_sqlite(nextgen_file, 'flowpath_attributes') %>% 
   collect() %>% 
   head()
+
+
+
+# Add some symbology ------------------------------------------------------
+
+
 
 append_style(nextgen_file, layer_names = c("divides", "flowpaths", "nexus"))
 
